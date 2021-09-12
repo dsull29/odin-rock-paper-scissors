00f7ffebd6c68f87bf7274cdb430a9ac3299ee70
David Sullivan <dsull829@gmail.com>
2021-09-12 17:23:24 -0400

    completed code

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
-const choices = ["Rock", "Paper", "Scissors"];
+const choices = ["rock", "paper", "scissors"];
 
 const computerPlay = () => choices[Math.floor(Math.random() * choices.length)];
 
 const playerPlay = () => {
   let answer = prompt(
     "Please enter either Rock, Paper, or Scissors: "
-  ).toString();
+  ).toString().toLowerCase();
 
   if (!choices.includes(answer)) {
     alert("Invalid response. Try again.");
@@ -15,41 +15,49 @@ const playerPlay = () => {
   return answer;
 };
 
-let playerSelection = playerPlay();
-
-let computerSelection = computerPlay();
-
 const playRound = (playerSelection, computerSelection) => {
   let playerResult = null;
-    if (playerSelection === computerSelection) {
-        playerResult = "Draw";
-        alert("Draw!")
-  } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+  if (playerSelection === computerSelection) {
+    playerResult = "Draw";
+    alert("Draw!");
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
     playerResult = "Loss";
-    alert("You Lose! Paper covers Rock.");
-      } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+    alert("You Lose! Paper covers rock.");
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
     playerResult = "Loss";
     alert("You Lose! Scissors cut paper.");
-  } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
     playerResult = "Loss";
     alert("You Lose! Rock smashes scissors.");
-  } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
     playerResult = "Win";
-    alert("You Win! Rock smashes Scissors.");
-  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    alert("You Win! Rock smashes scissors.");
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
     playerResult = "Win";
     alert("You Win! Paper covers rock.");
-  } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
     playerResult = "Win";
     alert("You Win! Scissors cut paper.");
   }
-  return playerResult
+  return playerResult;
 };
 
-let results = []
+let results = [];
 
-for (let i=0; i < 5; i++) {
- results.push(playRound(playerSelection,computerSelection))
-}
+const game = () => {
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = playerPlay();
+    let computerSelection = computerPlay();
+    results.push(playRound(playerSelection, computerSelection));
+  }
+  
+  let wins = results.filter(x => x === "Win").length
+  let losses = results.filter(x => x === "Loss").length 
+  let draws = results.filter(x => x === "Draw").length 
+  
+  return {wins, losses, draws}
+};
 
-console.log(results)
+const {wins, losses, draws} = game();
+
+alert(("W: " + wins + "\nL: " + losses + "\nD: " + draws))
